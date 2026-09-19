b800f00cc0ef6ef7add42e582167d4788ed08414
Andrew Via <andrew.n.via@gmail.com>
2026-09-18 21:10:23 -0400

    Fix placement of parallel in omp-ell

--- spmv-omp-ell.c
+++ spmv-omp-ell.c
@@ -37,9 +37,9 @@
 static void spmv_ell_rows(const ell_matrix * A, const csr_matrix * csr,
                           const float * x, float * y)
 {
-    #pragma omp parallel for schedule(runtime)
     (void)csr;
     const int K = A->max_row_len;
+    #pragma omp parallel for schedule(runtime)
     for (int i = 0; i < A->num_rows; i++) {
         const float * v = A->vals    + (size_t)i * K;
         const int   * c = A->col_idx + (size_t)i * K;
